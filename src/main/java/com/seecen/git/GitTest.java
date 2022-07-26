bb81524c53b3bce29ab0b8b8423858f47e15b7d5
zenghaolong <3281486327@qq.com>
2022-07-26 19:22:47 +0800

    third commit

--- src/main/java/com/seecen/git/GitTest.java
+++ src/main/java/com/seecen/git/GitTest.java
@@ -3,5 +3,6 @@ package com.seecen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
